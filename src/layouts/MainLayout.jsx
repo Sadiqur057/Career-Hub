8f308cd50be7ca83e09b2884b0c8b561a3045eed
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-03-24 11:01:16 +0600

    created main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
     <div>
       <h1>Hello</h1>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
